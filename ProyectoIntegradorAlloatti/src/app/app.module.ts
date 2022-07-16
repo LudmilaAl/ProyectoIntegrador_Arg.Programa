e1b12bd322e01af2ccc038e11380dcdab2302927
LudmilaAl <109355494+LudmilaAl@users.noreply.github.com>
2022-07-16 20:27:05 -0300

    FrontEnd

--- ProyectoIntegradorAlloatti/src/app/app.module.ts
+++ ProyectoIntegradorAlloatti/src/app/app.module.ts
@@ -3,14 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { AcercaComponent } from './components/acerca/acerca.component';
+import { ExplaboralComponent } from './components/explaboral/explaboral.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { MarcaComponent } from './components/marca/marca.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BannerComponent,
+    AcercaComponent,
+    ExplaboralComponent,
+    EducacionComponent,
+    SkillsComponent,
+    ProyectosComponent,
+    MarcaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
